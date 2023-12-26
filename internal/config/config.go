1fc738df57d206cfdb3e38e589b5ad3bb3231842
sbnemov <nemovsb@gmail.com>
2023-12-26 17:29:45 +0300

    add app config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,18 @@
 package config
+
+type Config struct {
+	Server  `mapstructure:"server"`
+	Storage `mapstructure:"storage"`
+}
+
+type Server struct {
+	Port string `mapstructure:"port"`
+}
+
+type Storage struct {
+	Host     string `mapstructure:"host"`
+	Port     string `mapstructure:"port"`
+	UserName string `mapstructure:"username"`
+	Password string `mapstructure:"storage_pswrd"`
+	DBName   string `mapstructure:"db_name"`
+}
